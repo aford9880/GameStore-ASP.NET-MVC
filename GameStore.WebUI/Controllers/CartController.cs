7657618dc6570e9bd97467a22ba57a6b7fce3464
aford9880 <aford9880@gmail.com>
2019-11-25 13:19:41 +0300

    Finished cart

--- GameStore.WebUI/Controllers/CartController.cs
+++ GameStore.WebUI/Controllers/CartController.cs
@@ -7,45 +7,40 @@ using GameStore.WebUI.Models;
 namespace GameStore.WebUI.Controllers {
     public class CartController : Controller {
 
-        public ViewResult Index(string returnUrl) {
-            return View(new CartIndexViewModel {
-                Cart = GetCart(),
-                ReturnUrl = returnUrl
-            });
-        }
-
         private IGameRepository repository;
+
         public CartController(IGameRepository repo) {
             repository = repo;
         }
-
-        public RedirectToRouteResult AddToCart(int gameId, string returnUrl) {
+        public ViewResult Index(Cart cart, string returnUrl) {
+            return View(new CartIndexViewModel {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+        
+        public RedirectToRouteResult AddToCart(Cart cart, int gameId, string returnUrl) {
             Game game = repository.Games
                 .FirstOrDefault(g => g.GameId == gameId);
 
             if (game != null) {
-                GetCart().AddItem(game, 1);
+                cart.AddItem(game, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl) {
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int gameId, string returnUrl) {
             Game game = repository.Games
                 .FirstOrDefault(g => g.GameId == gameId);
 
             if (game != null) {
-                GetCart().RemoveLine(game);
+                cart.RemoveLine(game);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public Cart GetCart() {
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null) {
-                cart = new Cart();
-                Session["Cart"] = cart;
-            }
-            return cart;
+        public PartialViewResult Summary(Cart cart) {
+            return PartialView(cart);
         }
     }
 }

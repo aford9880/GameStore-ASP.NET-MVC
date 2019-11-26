db1e7889df08f95d9434a07c330e7d912df68adc
aford9880 <aford9880@gmail.com>
2019-11-26 14:29:09 +0300

    Added images

--- GameStore.WebUI/Controllers/AdminController.cs
+++ GameStore.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using GameStore.Domain.Abstract;
 using GameStore.Domain.Entities;
@@ -24,8 +25,13 @@ namespace GameStore.WebUI.Controllers {
 
         // Перегруженная версия Edit() для сохранения изменений
         [HttpPost]
-        public ActionResult Edit(Game game) {
+        public ActionResult Edit(Game game, HttpPostedFileBase image = null) {
             if (ModelState.IsValid) {
+                if (image != null) {
+                    game.ImageMimeType = image.ContentType;
+                    game.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(game.ImageData, 0, image.ContentLength);
+                }
                 repository.SaveGame(game);
                 TempData["message"] = string.Format("Изменения в игре \"{0}\" были сохранены", game.Name);
                 return RedirectToAction("Index");

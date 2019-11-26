73f7d199ba6d3d5c9aa4b94c70b5bdb494b09091
aford9880 <aford9880@gmail.com>
2019-11-26 12:17:11 +0300

    Added verification and admin tools

--- GameStore.Domain/Concrete/EFGameRepository.cs
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -9,5 +9,29 @@ namespace GameStore.Domain.Concrete {
         public IEnumerable<Game> Games {
             get { return context.Games; }
         }
+
+        public Game DeleteGame(int gameId) {
+            Game dbEntry = context.Games.Find(gameId);
+            if (dbEntry != null) {
+                context.Games.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+
+        public void SaveGame(Game game) {
+            if (game.GameId == 0)
+                context.Games.Add(game);
+            else {
+                Game dbEntry = context.Games.Find(game.GameId);
+                if (dbEntry != null) {
+                    dbEntry.Name = game.Name;
+                    dbEntry.Description = game.Description;
+                    dbEntry.Price = game.Price;
+                    dbEntry.Category = game.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
